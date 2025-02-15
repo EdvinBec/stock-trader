e9f986ad8c2893b0e308ac1fbdfd5970956574b9
Edvin Becic <edvinbecic16@outlook.com>
2025-02-15 14:31:26 +0100

    feat (multiple stock tabs): add persist state in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,26 @@ import { Toaster } from "./components/ui/sonner.tsx";
 import StockPage from "./pages/StockPage.tsx";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import TabsRow from "./components/TabsRow.tsx";
 import NavigationBar from "./components/NavigationBar.tsx";
 import MaxWidthWrapper from "./components/MaxWidthWrapper.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <TabsRow />
-      <MaxWidthWrapper className="py-4">
-        <NavigationBar />
-      </MaxWidthWrapper>
-      <Routes>
-        <Route path="/" element={<StockPage />} />
-        <Route path="/s" element={<App />} />
-      </Routes>
-      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <TabsRow />
+        <MaxWidthWrapper className="py-4">
+          <NavigationBar />
+        </MaxWidthWrapper>
+        <Routes>
+          <Route path="/" element={<StockPage />} />
+          <Route path="/s" element={<App />} />
+        </Routes>
+        <Toaster />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,27 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
 import { tabsSlice } from "./features/tabsSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // default localStorage for the web
+
+// Persist configuration
+const persistConfig = {
+  key: "root", // key to identify the persisted state
+  storage, // which storage to use (localStorage in this case)
+};
+
+// Create the persisted reducer
+const persistedTabsReducer = persistReducer(persistConfig, tabsSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    tabs: tabsSlice.reducer,
+    tabs: persistedTabsReducer,
   },
+  // You can add middleware if needed, but the default is generally sufficient
 });
+
+// Persist the store
+export const persistor = persistStore(store);
 
 // Infer the type of `store`
 export type AppStore = typeof store;
